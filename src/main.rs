13adfa6b0bdc65456aba3bfa59d850781090edae
okwesi <okwesi73@gmail.com>
2023-01-10 22:30:21 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod arrays;
+
+fn main() {
+        arrays::arrays();
+        // Create an array of integers with 5 elements
+        let arr = [1, 2, 3, 4, 5];
+    
+        // Access an element by its index
+        println!("The second element is {}", arr[1]);
+    
+        // Iterate over the elements of the array
+        for x in arr.iter() {
+            println!("{}", x);
+        }
+    
+        // Create an array of strings with 3 elements
+        let names = ["Alice", "Bob", "Charlie"];
+    
+    
+        // Get the length of the array
+        println!("The array has {} elements", names.len());
+    }
+    
